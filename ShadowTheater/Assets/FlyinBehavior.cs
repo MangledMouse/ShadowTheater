a4e9b989aa90f04a4d3fe78e182ff6d59832d844
Chris <ctourlouki@gmail.com>
2022-01-29 13:36:36 -0500

    Just a single file for testing

---
+++ ShadowTheater/Assets/FlyinBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyinBehavior : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        //Finally!
+        var vertices = new Vector3[3];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
