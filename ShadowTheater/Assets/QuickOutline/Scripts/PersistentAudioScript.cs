19e655f3b31c6e6328c5ecf688295c889b518823
Chris <ctourlouki@gmail.com>
2022-01-30 12:51:22 -0500

    Play audio

---
+++ ShadowTheater/Assets/QuickOutline/Scripts/PersistentAudioScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PersistentAudioScript : MonoBehaviour
+{
+    public static PersistentAudioScript instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ShadowTheater/Assets/QuickOutline/Scripts/SplashScreenScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour
+{
+    float baseTimeToSwitchScene;
+    float currentTimeOnScreen;
+    public AudioSource IntroMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeOnScreen = 0f;
+        baseTimeToSwitchScene = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimeOnScreen += Time.deltaTime;
+        if (currentTimeOnScreen >= baseTimeToSwitchScene)
+            SwitchScene();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchScene();
+        }
+    }
+
+    void SwitchScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
